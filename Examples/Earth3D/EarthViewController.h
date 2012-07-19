32908d5aa7efd81586ce252455b760b0206fc963
Diego Marcos <diego.marcos@gmail.com>
2012-07-18 22:09:07 -0700

    Creates SEView SEViewController infrastructure

---
+++ Examples/Earth3D/EarthViewController.h
@@ -0,0 +1,13 @@
+//
+//  EarthViewController.h
+//  SparkEngine
+//
+//  Created by Diego Marcos on 7/18/12.
+//  Copyright (c) 2012 codebeast.org. All rights reserved.
+//
+
+#import "SEViewController.h"
+
+@interface EarthViewController : SEViewController
+
+@end
---
+++ SparkEngine/Core/SEViewController.m
@@ -0,0 +1,43 @@
+//
+//  SEViewController.m
+//  SparkEngine
+//
+//  Created by Diego Marcos on 7/18/12.
+//  Copyright (c) 2012 codebeast.org. All rights reserved.
+//
+
+#import "SEViewController.h"
+
+@interface SEViewController ()
+
+@end
+
+@implementation SEViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
